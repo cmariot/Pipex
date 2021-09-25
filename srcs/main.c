9c9b9039f99be205b8d518af69c370b95ad586ff
Charles Mariot <contact@charles-mariot.fr>
2021-09-25 12:25:08 +0200

    Initial fork

--- srcs/main.c
+++ srcs/main.c
@@ -6,47 +6,11 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 15:50:03 by cmariot           #+#    #+#             */
-/*   Updated: 2021/09/24 13:36:34 by cmariot          ###   ########.fr       */
+/*   Updated: 2021/09/25 12:23:49 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-void	restore_redirection1(int stdin_fd)
-{
-	dup2(stdin_fd, 0);
-	close(stdin_fd);
-}
-
-int	create_redirection1(char *file1)
-{
-	int	stdin_bckp;
-	int	fd;
-
-	stdin_bckp = dup(0);
-	fd = open(file1, O_RDONLY);
-	if (fd == -1)
-	{
-		ft_putstr("Error, the file1 can't be open.\n");
-		return (-1);
-	}
-	dup2(fd, 0);
-	close(fd);
-	return (stdin_bckp);
-}
-
-int	execute_cmd1_on_file1(char *file1, char *cmd1)
-{
-	int	stdin_fd;
-
-	//create redirection fd -> in
-	stdin_fd = create_redirection1(file1);
-	//execute cmd1
-	ft_putstr_fd(cmd1, stdin_fd);
-	//restore redirection
-	restore_redirection1(stdin_fd);
-	return (0);
-}
 
 int	main(int argc, char **argv, char **env)
 {
@@ -54,6 +18,8 @@ int	main(int argc, char **argv, char **env)
 	char	*cmd1;
 	char	*cmd2;
 	char	*file2;
+	int		fd[2];
+	int		pid;
 
 	file1 = argv[1];
 	cmd1 = argv[2];
@@ -61,8 +27,46 @@ int	main(int argc, char **argv, char **env)
 	file2 = argv[4];
 	if (argc != 0)
 	{
-		execute_cmd1_on_file1(file1, cmd1);	
-		execute_cmd2_in_file2(cmd2, file2, env);
+		pipe(fd);
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			return (-1);
+		}
+		if (pid == 0)
+		{
+			printf("I'm the child, my pid is %d\n", pid);
+			int	stdin_bckp;
+			int	file1_fd;
+
+			stdin_bckp = dup(0);
+			file1_fd = open(argv[1], O_RDONLY);
+			if (file1_fd == -1)
+			{
+				perror("open file1");
+				return (-1);
+			}
+			dup2(file1_fd, 0);
+			close(file1_fd);
+			
+			char	*line;
+			while (1)
+			{
+				line = get_next_line(file1_fd);
+				ft_putstr_fd(line, file1_fd);
+				if (line == NULL)
+					break ;
+				free(line);
+			}
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			wait(NULL);
+			printf("I'm the parent, my pid is %d\n", getpid());
+			execute_cmd2_in_file2(cmd2, file2, env);
+		}
 	}
 	return (0);
 }
